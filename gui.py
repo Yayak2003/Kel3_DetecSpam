1088b3f9ac69e91cb8093da268984839269b4436
hasbihd <123924321+thesslow@users.noreply.github.com>
2024-03-31 14:38:57 +0700

    Update gui.py
    adding column for streamlit web interface

--- gui.py
+++ gui.py
@@ -4,12 +4,11 @@ from keras.preprocessing.sequence import pad_sequences
 import pickle
 import numpy as np
 
-# Load the pre-trained LSTM model and tokenizer
-model = load_model('spam_classifier_model.h5')
-with open('tokenizer.pickle', 'rb') as handle:
+col1, col2, col3 = st.columns([1, 3, 1])
+model = load_model('model/spam_classifier_LSTM_30e_model.h5')
+with open('model/tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
-
-# Function to preprocess text and make predictions
+    
 def classify_message(message):
     max_len = 150  # Same as during training
     sequence = tokenizer.texts_to_sequences([message])
@@ -17,7 +16,6 @@ def classify_message(message):
     prediction = model.predict(padded_sequence)
     return prediction[0][0]  # Assuming model outputs a single probability score
 
-# Function to check for fraud characteristics in the message
 def check_fraud_indicators(message):
     fraud_indicators = [
         "bank", "nomor", "kartu kredit", "kata sandi", "nomor rekening",
@@ -31,24 +29,28 @@ def check_fraud_indicators(message):
             return True
     return False
 
-# Streamlit app
-st.title('DETEKSI SPAM PENIPUAN')
-user_input = st.text_area("Masukkan sebuah pesan:")
-
-if st.button('Cek'):
-    prediction_score = classify_message(user_input)
-    is_fraud = check_fraud_indicators(user_input)
-    
-    if prediction_score > 0.5:
-        if is_fraud:
-            st.error("Pesan ini adalah spam penipuan")
-            st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
+with col2:
+    st.header("SPAM Classifier")
+    user_input = st.text_area("Masukkan sebuah pesan:")
+    if st.button('Check'):
+        prediction_score = classify_message(user_input) 
+        is_fraud = check_fraud_indicators(user_input)
+        
+        if prediction_score > 0.5:
+            if is_fraud:
+                st.error("Pesan ini adalah spam penipuan")
+                st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
+            else:
+                st.warning("Pesan ini adalah spam biasa")
+                st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
         else:
-            st.warning("Pesan ini adalah spam biasa")
+            st.success("Pesan ini adalah ham (bukan spam)")
             st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
-    else:
-        st.success("Pesan ini adalah ham (bukan spam)")
-        st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
-    
-    # Displaying the prediction confidence
-    # st.write(f"Accuracy: {np.max(prediction_score) * 100:.2f}%")
+
+with col1:
+    st.header("+++++++")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col3:
+    st.header("+++++++")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
